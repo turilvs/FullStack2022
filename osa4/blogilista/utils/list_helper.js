33a8bdefe2cf0124956aa156cad96bdb922e709e
Rantala Artturi Kalle Nestori <artturi.rantala@hotmail.com>
2022-05-29 10:26:58 +0300

    muokattu tekematta.txt

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,25 @@
+const dummy = (blogs) => {
+    return Number(1)
+  }
+  
+
+const totalLikes = (blogs) => {
+
+    var likes = blogs.reduce((sum, blog) =>  sum + blog.likes, 0)
+    return blogs.length === 0 ? 0: likes
+}
+
+const favoriteBlog = (blogs) => {
+
+    var mostLikes = blogs.reduce((most, current) => most.likes > current.likes ? most : current);
+    delete mostLikes['__v']
+    delete mostLikes['_id']
+    delete mostLikes['url']
+    return mostLikes
+}
+
+  module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+  }
